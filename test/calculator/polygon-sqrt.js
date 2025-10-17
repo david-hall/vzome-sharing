c81f0b9b9e017bc2e9b64c637ffbc3b704a26851
david-hall <David.42.Hall@gmail.com>
2025-10-17 16:57:37 -0400

    Refactor polygon-sqrt calculator

--- test/calculator/polygon-sqrt.js
+++ test/calculator/polygon-sqrt.js
@@ -18,172 +18,112 @@ export class SqrtCalculator extends HTMLElement {
     }
 
     initialize( radicand ) {
-        vZomeLegacyIsReady()
-            .then( core => {
-                const field = core .getField( "polygon" + (2*radicand) );
-                if ( field.unknown ) {
-                    console .dir( field );
-                    return; // don't change anything
-                }
-                console.log(field.getName());
-                let den = field.one(); // same as getUnitDiagonal(0);
-                let num = field.getUnitDiagonal(1);
-                // diags 0 and 1 are the initial values, 
-                // so start the loop with diags 2 & 3
-                const diagLimit = radicand - 1;
-                const fmt = 4; // MATHML
-                let sFractions = `<mo>(</mo>
-                                <mfrac>
-                                    <mrow>${num.toString(fmt)}</mrow>
-                                    <mrow>${den.toString(fmt)}</mrow>
-                                </mfrac>
-                            <mo>)</mo>`
-                let sFactors = `<mo>(</mo>
-                                <mrow>${num.toString(fmt)}</mrow>
-                            <mo>)</mo>`
-                for(let diag = 2; diag < diagLimit; diag += 2) {
-                    // Multiply all diags with even indices to get the denominator
-                    let d = field.getUnitDiagonal(diag);
-                    // Multiply all diags with odd  indices to get the numerator
-                    let n = field.getUnitDiagonal(diag+1);
-                    den = den.times(d);
-                    num = num.times(n);
-                    sFractions += `<mo>(</mo>
-                                <mfrac>
-                                    <mrow>${n.toString(fmt)}</mrow>
-                                    <mrow>${d.toString(fmt)}</mrow>
-                                </mfrac>
-                            <mo>)</mo>`
-                    sFactors += `<mo>(</mo>
-                                <mrow>${n.dividedBy(d).toString(fmt)}</mrow>
-                            <mo>)</mo>`
-                }
-                const result = num.dividedBy(den);
-                let sRes = result.toString(fmt);
-                const r = result.evaluate();
-                this.#container.innerHTML = 
-                    `<math display="block">
-                        <mtext>
-                            Derive &nbsp;
-                        </mtext>
-                        <msqrt><mtext>${radicand}</mtext></msqrt>
-                        <mtext>
-                            &nbsp; from the ${field.order-1} shortest diagonals 
-                            of a regular (2*${(radicand)})=${(2*radicand)}-gon with an edge length of 1.
-                        </mtext>
-                     </math>
-                     <hr>
-                     <math display="block">
-                        <mrow>
-                            <mi>r</mi><mo>≥</mo><mn>2</mn><mtext>,&nbsp;</mtext>
-                            <mi>r</mi><mo>∈</mo><mtext>&Nopf;&nbsp;</mtext>
-                            <!--mo stretchy="false" lspace="0em" rspace="0em">⇒</mo-->
-                        <mrow>
-                    </math>
-                     <br>
-                     <math display="block">
-    <mrow>
-      <msqrt><mi>r</mi></msqrt><mo>=</mo>
-      <munderover>
-        <mo movablelimits="false">∏</mo>
-        <mrow>
-          <mi>k</mi>
-          <mo>=</mo>
-          <mn>1</mn>
-        </mrow>
-        <mrow>
-          <mo fence="true" form="prefix">⌊</mo>
-          <mfrac>
-            <mi>r</mi>
-            <mn>2</mn>
-          </mfrac>
-          <mo fence="true" form="postfix">⌋</mo>
-        </mrow>
-      </munderover>
-    </mrow>
-    <mfrac>
-      <mrow>
-        <mi>s</mi>
-        <mi>i</mi>
-        <mi>n</mi>
-        <mo>(</mo>
-        <mfrac>
-          <mrow>
-            <mn>2</mn>
-            <mi>k</mi>
-            <mi>π</mi>
-          </mrow>
-          <mrow>
-            <mn>2</mn>
-            <mi>r</mi>
-          </mrow>
-        </mfrac>
-        <mo>)</mo>
-      </mrow>
-      <mrow>
-        <mi>s</mi>
-        <mi>i</mi>
-        <mi>n</mi>k
-        <mo>(</mo>
-        <mfrac>
-          <mrow>
-            <mo>(</mo>
-            <mn>2</mn>
-            <mi>k</mi>
-            <mo>-</mo>
-            <mn>1</mn>
-            <mo>)</mo>
-            <mi>π</mi>
-          </mrow>
-          <mrow>
-            <mn>2</mn>
-            <mi>r</mi>
-          </mrow>
-        </mfrac>
-        <mo>)</mo>
-      </mrow>
-    </mfrac>
-  </mrow>
-                        </mrow>
-                     </math>
-                     <hr>
-                     <math display="block">
-                        <mrow>
-                            <mrow>${sFractions}</mrow>
-                        </mrow>
-                     </math>
-                     <br>
-                     <math display="block">
-                        <mrow>
-                            <mo>=</mo>
-                            <mrow>${sFactors}</mrow>
-                        </mrow>
-                     </math>
-                     <br>
-                     <math display="block">
-                        <mrow>
-                            <mo>=</mo>
-                            <mn>${sRes}</mn>
-                        </mrow>
-                     </math>
-                     <br>
-                     <math display="block">
-                        <mrow>
-                            <mo>=</mo>
-                            <mn>${r}</mn>
-                        </mrow>
-                     </math>
-                     <br>
-                     <math display="block">
-                        <mrow>
-                            <mo>=</mo>
-                            <msqrt><mn>${radicand}</mn></msqrt>
-                        </mrow>
-                     </math>
-                     <hr>
-                     `;
-            })
-	}
+        vZomeLegacyIsReady() .then( core => {
+            const field = core .getField( "polygon" + (2*radicand) );
+            if ( field.unknown ) {
+                console .dir( field );
+                return; // don't change anything
+            }
+            console.log(field.getName());
+            this.showSqrt(radicand, field);
+        });
+    }
+
+    showSqrt(radicand, field) {
+        const fmt = 4; // MATHML
+        let den = field.one(); // same as getUnitDiagonal(0);
+        let num = field.getUnitDiagonal(1);
+        // diags 0 and 1 are the initial values, 
+        // so start the loop with diags 2 & 3
+        const diagLimit = radicand - 1;
+        let sFractions = 
+            `<mo>(</mo>
+                <mfrac>
+                    <mrow>${num.toString(fmt)}</mrow>
+                    <mrow>${den.toString(fmt)}</mrow>
+                </mfrac>
+            <mo>)</mo>`
+        let sFactors = 
+            `<mtr>
+                <mtd>
+                    <mo>=</mo>
+                    <mo>(</mo>
+                </mtd><mtd>
+                    <mrow>${num.toString(fmt)}</mrow>
+                </mtd><mtd>
+                    <mo>)</mo>
+                </mtd>
+            </mtr>`
+        for(let diag = 2; diag < diagLimit; diag += 2) {
+            // Multiply all diags with even indices to get the denominator
+            const d = field.getUnitDiagonal(diag);
+            // Multiply all diags with odd  indices to get the numerator
+            const n = field.getUnitDiagonal(diag+1);
+            den = den.times(d);
+            num = num.times(n);
+            sFractions += 
+                `<mo>(</mo>
+                    <mfrac>
+                        <mrow>${n.toString(fmt)}</mrow>
+                        <mrow>${d.toString(fmt)}</mrow>
+                    </mfrac>
+                <mo>)</mo>`
+            sFactors += 
+                `<mtr>
+                    <mtd>
+                        <mo>&times;</mo>
+                        <mo>(</mo>
+                    </mtd><mtd>
+                        <mrow>${n.dividedBy(d).toString(fmt)}</mrow>
+                    </mtd><mtd>
+                        <mo>)</mo>
+                    </mtd>
+                </mtr>`
+        }
+        const result = num.dividedBy(den);
+        let sRes = result.toString(fmt);
+        const r = result.evaluate();
+        this.#container.innerHTML = 
+            `<math display="block">
+                <mtext>Derive &nbsp;</mtext>
+                <msqrt><mtext>${radicand}</mtext></msqrt>
+                <mtext>
+                    &nbsp; from the ${field.order-1} shortest diagonals 
+                    of a regular ${(2*radicand)}-gon 
+                    having an edge length of 1.
+                </mtext>
+            </math>
+            <hr>
+            <math display="block">
+                <mrow>${sFractions}</mrow>
+            </math>
+            <br>
+            <math display="block">
+                <mtable>${sFactors}</mtable>
+            </math>
+            <br>
+            <math display="block">
+                <mrow>
+                    <mo>=</mo>
+                    <mn>${sRes}</mn>
+                </mrow>
+            </math>
+            <br>
+            <math display="block">
+                <mrow>
+                    <mo>=</mo>
+                    <mn>${r}</mn>
+                </mrow>
+            </math>
+            <br>
+            <math display="block">
+                <mrow>
+                    <mo>=</mo>
+                    <msqrt><mn>${radicand}</mn></msqrt>
+                </mrow>
+            </math>
+            <hr>`
+    }
 }
 
 customElements.define( "sqrt-calculator", SqrtCalculator );
