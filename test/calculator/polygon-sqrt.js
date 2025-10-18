1171e4511f6825c143d6dccf0389477b669069ba
david-hall <David.42.Hall@gmail.com>
2025-10-17 20:01:06 -0400

    Add user input, HTML attribute and queryString to specify radicand

--- test/calculator/polygon-sqrt.js
+++ test/calculator/polygon-sqrt.js
@@ -3,9 +3,11 @@ import { initialize as vZomeLegacyIsReady } from "https://www.vzome.com/modules/
 export class SqrtCalculator extends HTMLElement {
     #root;
     #container;
+    #radicand;
 
     constructor() {
         super();
+        this.#radicand = 2;
         this.#root = this.attachShadow({ mode: "open" });
         //this.#root.appendChild( document.createElement("style") ).textContent = SqrtCalculatorCSS;
         this.#container = document.createElement("div");
@@ -14,28 +16,28 @@ export class SqrtCalculator extends HTMLElement {
     }
 
     connectedCallback() {
-        this.initialize(18);
+        this.initialize();
     }
 
-    initialize( radicand ) {
+    initialize() {
         vZomeLegacyIsReady() .then( core => {
-            const field = core .getField( "polygon" + (2*radicand) );
+            const field = core .getField( "polygon" + (2*this.radicand) );
             if ( field.unknown ) {
                 console .dir( field );
                 return; // don't change anything
             }
             console.log(field.getName());
-            this.showSqrt(radicand, field);
+            this.showSqrt(field);
         });
     }
 
-    showSqrt(radicand, field) {
+    showSqrt(field) {
         const fmt = 4; // MATHML
         let den = field.one(); // same as getUnitDiagonal(0);
         let num = field.getUnitDiagonal(1);
         // diags 0 and 1 are the initial values, 
         // so start the loop with diags 2 & 3
-        const diagLimit = radicand - 1;
+        const diagLimit = this.radicand - 1;
         let sFractions = 
             `<mo>(</mo>
                 <mfrac>
@@ -85,11 +87,11 @@ export class SqrtCalculator extends HTMLElement {
         const r = result.evaluate();
         this.#container.innerHTML = 
             `<math display="block">
-                <mtext>Derive &nbsp;</mtext>
-                <msqrt><mtext>${radicand}</mtext></msqrt>
+                <mtext>Solve for &nbsp;</mtext>
+                <msqrt><mtext>${this.radicand}</mtext></msqrt>
                 <mtext>
-                    &nbsp; from the ${field.order-1} shortest diagonals 
-                    of a regular ${(2*radicand)}-gon 
+                    &nbsp; using the ${field.order-1} shortest diagonals 
+                    of a regular ${(2*this.radicand)}-gon 
                     having an edge length of 1.
                 </mtext>
             </math>
@@ -119,11 +121,45 @@ export class SqrtCalculator extends HTMLElement {
             <math display="block">
                 <mrow>
                     <mo>=</mo>
-                    <msqrt><mn>${radicand}</mn></msqrt>
+                    <msqrt><mn>${this.radicand}</mn></msqrt>
                 </mrow>
             </math>
             <hr>`
     }
+
+    // Called when HTML attributes are initialized or changed
+    // which is different from directly calling corresponding setters on this object
+	attributeChangedCallback( attributeName, oldValue, newValue ) {
+        console.dir({attributeName, oldValue, newValue });
+        switch (attributeName) {
+        case "radicand":
+            // call the setter so all validation is in one place.
+            this.radicand = newValue;
+            break;
+        default:
+            console.dir({attributeName, oldValue, newValue });
+        }
+	}
+
+    get radicand() {
+		return this.#radicand;
+	}
+	set radicand(newValue) {
+        if(newValue>=2 && newValue<=40) {
+		    this.#radicand = newValue;
+            this.initialize();
+        } else {
+            console.log(`radicand ${newValue} is out of range.`);
+        }
+	}
+
+    static get observedAttributes() {
+        // Initial priority is 
+        // 1) URL queryString 
+        // 2) HTML attribute 
+        // 3) default value = 2
+		return [ "radicand" ];
+	}
 }
 
 customElements.define( "sqrt-calculator", SqrtCalculator );
