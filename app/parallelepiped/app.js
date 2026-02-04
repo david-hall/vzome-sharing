7edc265b321f069c158d22c91a34c1554d8df167
david-hall <David.42.Hall@gmail.com>
2026-02-03 23:10:58 -0500

    Cleanup templates so downloaded files work with desktop
    Trim template before calling loadFromText().
    Limit editNumber to not exceed the total edits pre template. The viewer doesn't care, but desktop is a bit pickier.
    The 1 and 2 direction templates are now centered around the origin before their final edit. The 3 direction template was already centered well before its final edit.

--- app/parallelepiped/app.js
+++ app/parallelepiped/app.js
@@ -17,10 +17,20 @@ async function refresh() {
     const dims = directions.size
     let template = document.getElementById(`template-${dims}`).textContent
 
-    // Adjust editNumber. It's OK for it to be larger than the total number of edits in the template.
-    const editNumber = document.getElementById("editNumber").value
-    template = template.replace(`editNumber="0"`, `editNumber="${editNumber}"`)
-
+    const regex = /editNumber="(\d+)"/
+    const match = template.match(regex)
+    if (match && match.length > 1) {
+        const oldEditNumber = match[1] // 
+        console.log(oldEditNumber)
+        // It's OK for the viewer if editNumber is larger than the total number of edits,
+        // but I'll limit it in case the file is downloaded to desktop, which is a bit pickier.
+        const newEditNumber = document.getElementById("editNumber").value
+        if(+newEditNumber <= +oldEditNumber) { // use + prefix ensure numeric rather than alphabetic comparison
+            // Adjust editNumber
+            template = template.replace(`editNumber="${oldEditNumber}"`, `editNumber="${newEditNumber}"`)
+        }
+    }
+    
     // Adjust template directions
     directionValues.forEach((value, i) => {
         const {orbit, axis, len, zomic} = value
@@ -39,6 +49,12 @@ async function refresh() {
     // const newLk = `<LookDirection x="-0.5773502588272095" y="-0.5773502588272095" z="-0.5773502588272095"/>`
     // template = template.replaceAll(oldLk, newLk).replaceAll(oldUp, newUp)
 
+    // Trimming whitespace is not necessary for the viewer to render,
+    // but if it's not trimmed, then a vZome file downloaded from the current version of the viewer
+    // will fail to opened in desktop because that XML parser can't handle leading whitespace.
+    // TODO: The viewer itself should trim anything rec'd by loadFromText()
+    template = template.trim() 
+    
     const filename = `Parallelepiped-${directionValues.map(value => `${value.orbit}_${value.axis}`).join('-')}.vZome`
     // update the viewer
     const viewer = document.getElementById("viewer")
